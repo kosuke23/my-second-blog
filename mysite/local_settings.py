fc9e383537a37e7cf3a51039ced8f91a8852a2f6
Your Name <you@example.com>
2020-07-12 16:45:27 +0900

    modified wsgi and settings for Heroku

--- mysite/local_settings.py
+++ mysite/local_settings.py
@@ -82,8 +82,8 @@ DATABASES = {
         #  'PASSWORD': '',
         #  'HOST': 'localhost',
         #  'PORT': '',
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
